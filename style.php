7d5b0a6e7ec8df6d08971c5b1a5c72f85661b2af
Jitendra Gaur <jitendra.gaur@me.com>
2013-01-23 14:51:29 +0530

    style.php removed and styles.css added

--- style.php
+++
@@ -1,8 +0,0 @@
-.block_my_unread_forum_posts .forum_unread {
-    margin-left: 3em !important;
-    background: #FFEB80 !important;    
-    padding:2px !important;
-}
-.block_my_unread_forum_posts .forum_unread a:hover {    
-    text-decoration: none !important;
-}
